7db2d3c51b6bffbf61cfa4de03dd6a46065f7990
vipgio <71258508+vipgio@users.noreply.github.com>
2022-11-25 14:35:28 +0330

    added bottleneck

--- pages/api/rush/play-game.js
+++ pages/api/rush/play-game.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import axiosRateLimit from "axios-rate-limit";
-const http = axiosRateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 7000 });
+import Bottleneck from "bottleneck";
+const http = axiosRateLimit(axios.create(), { maxRequests: 120, perMilliseconds: 60000 });
+const limiter = new Bottleneck({
+	maxConcurrent: 1,
+	minTime: 7000,
+});
+
 const { API } = require("@/config/config");
 
 export default async function handler(req, res) {
@@ -24,7 +30,10 @@ export default async function handler(req, res) {
 				},
 			});
 		};
-		const { data } = await playGame(jwt, rosterId, enemyRosterId, bannedMapIds);
+
+		const { data } = await limiter.schedule(() =>
+			playGame(jwt, rosterId, enemyRosterId, bannedMapIds)
+		);
 		res.status(200).json(data);
 	} catch (err) {
 		console.log(err);
