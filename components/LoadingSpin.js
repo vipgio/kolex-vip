2e965f1b8a7e6c3bc88c98743c22de6b935ed44f
vipgio <e.nabavi78@gmail.com>
2024-08-31 13:33:07 +0200

    new loading animation

--- components/LoadingSpin.js
+++ components/LoadingSpin.js
@@ -1,10 +1,6 @@
 const LoadingSpin = ({ size }) => {
 	return (
-		<div
-			className={`${
-				size === 4 ? "h-4 w-4" : "h-7 w-7"
-			} animate-spin rounded-full border-4 border-gray-200 border-t-gray-700`}
-		></div>
+		<div className={`${size === 4 ? "h-4 w-4" : "h-7 w-7"} loader h-7 w-7 animate-spin rounded-full`}></div>
 	);
 };
 export default LoadingSpin;
