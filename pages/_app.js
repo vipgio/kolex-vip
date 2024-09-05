26db52514a91150ea9d7525720fa2fdbf005f2a0
vipgio <e.nabavi78@gmail.com>
2024-09-05 23:29:27 +0200

    vercel speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+import { useRouter } from "next/router";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "../globals.css";
 import UserContextProvider from "@/context/UserContext";
 import ThemeContextProvider from "@/context/ThemeContext";
@@ -8,6 +10,7 @@ import PremiumRoutes from "@/HOC/PremiumRoutes";
 import Layout from "@/components/Layout";
 
 const App = ({ Component, pageProps }) => {
+	const router = useRouter();
 	const protectedRoutes = [
 		"/cardlister",
 		"/circulation",
@@ -44,6 +47,7 @@ const App = ({ Component, pageProps }) => {
 							<PrivateRoute protectedRoutes={protectedRoutes}>
 								<PremiumRoutes premiumRoutes={premiumRoutes}>
 									<Component {...pageProps} />
+									<SpeedInsights route={router.pathname} />
 								</PremiumRoutes>
 							</PrivateRoute>
 						</Layout>
