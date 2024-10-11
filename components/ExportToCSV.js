edfa7ed78841e1ba373c5ec13d4db032d9101e0f
vipgio <e.nabavi78@gmail.com>
2024-10-11 11:49:59 +0200

    login code change, template id export

--- components/ExportToCSV.js
+++ components/ExportToCSV.js
@@ -1,115 +1,156 @@
 import { CSVLink } from "react-csv";
 import { webApp } from "@/config/config";
 const ExportToCSV = ({ data, filename, type }) => {
-	const headers = {
-		mint: ["Mint Batch", "Mint Number", "Title", "Owner", "ID", "Signed", "Points", "Point gain"],
-		market: [
-			"Mint Batch",
-			"Mint Number",
-			"Title",
-			"Seller",
-			"ID",
-			"Signed",
-			"Type",
-			"Points",
-			"Point gain",
-			"Min Offer",
-			"Price",
-			"Link",
-		],
-		compact: ["Best Mint", "Title", "Owned", "Circulation"],
-		full: [
-			"Mint Batch",
-			"Mint Number",
-			"Title",
-			"Circulation",
-			"Listed",
-			"Market ID",
-			"Immutable",
-			"ID",
-			"Signed",
-			"Owner",
-			"Points",
-			"Need",
-			"Point Gain",
-		],
-		transaction: ["Date", "Description", "Amount", "Type", "Details", "Cost Type", "ID"],
+	const commonHeaders = ["Mint Batch", "Mint Number", "Title", "Template ID"];
+	const defineHeaders = () => {
+		return {
+			mint: [...commonHeaders, "Owner", "ID", "Signed", "Points", "Point gain"],
+			market: [
+				...commonHeaders,
+				"Seller",
+				"ID",
+				"Signed",
+				"Type",
+				"Points",
+				"Point gain",
+				"Min Offer",
+				"Price",
+				"Link",
+			],
+			compact: [...commonHeaders, "Owned", "Circulation"],
+			full: [
+				...commonHeaders,
+				"Circulation",
+				"Listed",
+				"Market ID",
+				"Immutable",
+				"ID",
+				"Signed",
+				"Owner",
+				"Points",
+				"Need",
+				"Point Gain",
+			],
+			transaction: ["Date", "Description", "Amount", "Type", "Details", "Cost Type", "ID"],
+		};
 	};
 
-	const array =
-		type === "mint"
-			? data.map((item) => [
-					//mint search
-					item.mintBatch,
-					item.mintNumber,
-					item.title,
-					item.owner.username,
-					item.id,
-					item.signatureImage ? "Yes" : "No",
-					(item.rating * 10).toFixed(2),
-					item.delta,
-			  ])
-			: type === "market"
-			? data.map((item) => [
-					//market search
-					item[item.type].mintBatch,
-					item[item.type].mintNumber,
-					item.title,
-					item.user.username,
-					item.card ? item.card.id : item.sticker.id,
-					item.card ? (item.card.signatureImage ? "Yes" : "No") : "No",
-					item.type,
-					(item[item.type].rating * 10).toFixed(2),
-					item.delta,
-					item.minOffer ? item.minOffer : "-",
-					item.price,
-					`${webApp}/${item.type}/${item.templateUUID}/${item.uuid}`,
-			  ])
-			: type === "compact"
-			? data.map((item) => [
-					//scanner compact
-					item.mintBatch,
-					item.mintNumber,
-					item.title,
-					item.owned,
-					item.inCirculation,
-			  ])
-			: type === "full"
-			? data.map((item) => [
-					//scanner
-					item.mintBatch,
-					item.mintNumber,
-					item.title,
-					item.inCirculation,
-					item.status === "market" ? "Yes" : "No",
-					item.marketId,
-					item.status === "imx_locked" ? "Yes" : "No",
-					item.id,
-					item.signed ? "Yes" : "No",
-					item.owner,
-					(item.rating * 10).toFixed(2),
-					item.need ? "Yes" : "No",
-					Math.max(item.delta, 0),
-			  ]) //transaction
-			: data.map((item) => [
-					item.created.replace("T", " ").split(".")[0],
-					item.description,
-					item.amount,
-					item.type,
-					item.details?.title ? item.details.title : "-",
-					item.costType,
-					item.details?.entityId ? item.details.entityId : "-",
-			  ]);
+	const headers = defineHeaders();
+
+	const mapData = (type, data) => {
+		const mappingFunctions = {
+			mint: (item) => {
+				const { mintBatch, mintNumber, title, id, owner, signatureImage, rating, delta } = item;
+				const templateId = item.templateId || item.cardTemplateId || item.stickerTemplateId;
+				return [
+					mintBatch,
+					mintNumber,
+					title,
+					templateId,
+					owner.username,
+					id,
+					signatureImage ? "Yes" : "No",
+					(rating * 10).toFixed(2),
+					delta,
+				];
+			},
+			market: (item) => {
+				const {
+					type: itemType,
+					title,
+					user,
+					card,
+					sticker,
+					delta,
+					minOffer,
+					price,
+					templateUUID,
+					uuid,
+				} = item;
+				const itemTypeData = item[itemType];
+				const templateId = item.templateId || itemTypeData.cardTemplateId || itemTypeData.stickerTemplateId;
+				return [
+					itemTypeData.mintBatch,
+					itemTypeData.mintNumber,
+					title,
+					templateId,
+					user.username,
+					card ? card.id : sticker.id,
+					card ? (card.signatureImage ? "Yes" : "No") : "No",
+					itemType,
+					(itemTypeData.rating * 10).toFixed(2),
+					delta,
+					minOffer || "-",
+					price,
+					`${webApp}/${itemType}/${templateUUID}/${uuid}`,
+				];
+			},
+			compact: ({ mintBatch, mintNumber, title, templateId, owned, inCirculation }) => [
+				mintBatch,
+				mintNumber,
+				title,
+				templateId,
+				owned,
+				inCirculation,
+			],
+			full: (item) => {
+				const {
+					mintBatch,
+					mintNumber,
+					title,
+					templateId,
+					inCirculation,
+					status,
+					marketId,
+					id,
+					signed,
+					owner,
+					rating,
+					need,
+					delta,
+				} = item;
+				return [
+					mintBatch,
+					mintNumber,
+					title,
+					templateId,
+					inCirculation,
+					status === "market" ? "Yes" : "No",
+					marketId,
+					status === "imx_locked" ? "Yes" : "No",
+					id,
+					signed ? "Yes" : "No",
+					owner,
+					(rating * 10).toFixed(2),
+					need ? "Yes" : "No",
+					Math.max(delta, 0),
+				];
+			},
+			transaction: (item) => {
+				const { created, description, amount, type, details, costType } = item;
+				return [
+					created.replace("T", " ").split(".")[0],
+					description,
+					amount,
+					type,
+					details?.title || "-",
+					costType,
+					details?.entityId || "-",
+				];
+			},
+		};
+
+		const mappingFunction = mappingFunctions[type] || mappingFunctions.transaction;
+		return data.map(mappingFunction);
+	};
+
+	const array = mapData(type, data);
 
 	const csvData = [headers[type], ...array];
 	return (
-		<div>
-			<button className='button'>
-				<CSVLink data={csvData} filename={filename} tabIndex={-1}>
-					Export
-				</CSVLink>
-			</button>
-		</div>
+		<CSVLink data={csvData} filename={filename} tabIndex={-1}>
+			<button className='button'>Export</button>
+		</CSVLink>
 	);
 };
 export default ExportToCSV;
